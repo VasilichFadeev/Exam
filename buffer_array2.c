464552131364e4e4db5fb330a5c3fd1e07e758cc
Pandoras_ubuntu <keksonianin@gmail.com>
2026-07-17 12:13:50 +0700

    clang-formatted

--- buffer_array2.c
+++ buffer_array2.c
@@ -2,9 +2,9 @@
 #include <stdlib.h>
 
 int main(void) {
-  int capacity = 4;  // начальный размер массива
-  int length = 0;    // текущий размер массива
   int flag = 0;
+  int capacity = 2;
+  int length = 0;
   int *array = (int *)malloc(capacity * sizeof(int));
   if (array == NULL) {
     flag = 1;
@@ -23,21 +23,33 @@ int main(void) {
     }
     if (flag == 0) {
       array[length++] = num;
-      if (ch == '\n') break;
+      if (ch == '\n') {
+        break;
+      }
+    }
+    if (flag == 0 && length > 0) {
+      int c;
+      do {
+        c = getchar();
+      } while (c == ' ' || c == '\t');
+      if (c != '\n' && c != EOF) {
+        flag = 1;
+      }
     }
   }
   if (flag == 0 && length > 0) {
     for (int i = 0; i < length; i++) {
-      if (i != length - 1) {
-        printf("%d ", array[i]);
-      } else {
+      if (i == length - 1) {
         printf("%d", array[i]);
+      } else {
+        printf("%d ", array[i]);
       }
     }
+  } else {
+    flag = 1;
   }
   if (flag == 1) {
     printf("n/a");
   }
   free(array);
-  return flag;
 }
