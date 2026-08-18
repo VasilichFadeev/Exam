909ff46c73ade10b3999b544a43d90b2b07ec6eb
Pandora Sanjuana <pandoras@si-c5.nsk.21-school.ru>
2026-08-18 23:39:53 +0700

    Updated program

--- sentence.c
+++ sentence.c
@@ -2,39 +2,42 @@
 #include <string.h>
 
 int count_sentences(char str[1000]) {
-    int counter = 0;
-    int i = 0;
-    while (str[i] != '\0') {
-        if (str[i] == '.' || str[i] == '!' || str[i] == '?') {
-            counter++;
-            i++;
-            while (str[i] == '.' || str[i] == '?' || str[i] == '!') {
-                i++;
-            }
-        } else {
-            i++;
-        }
+  int counter = 0;
+  int i = 0;
+  while (str[i] != '\0') {
+    if (str[i] == '.' || str[i] == '!' || str[i] == '?') {
+      counter++;
+      i++;
+      while (str[i] == '.' || str[i] == '?' || str[i] == '!') {
+        i++;
+      }
+    } else {
+      i++;
     }
-    return counter;
+  }
+  return counter;
 }
 
 int main(void) {
-    int flag = 0;
-    char name[1000];
-    fgets(name, sizeof(name), stdin);
-    name[strcspn(name, "\n")] = '\0';
-    char str[1000];
-    FILE *file;
-    file = fopen(name, "r");
-    if (file == NULL) {
-        flag = 1;
-    } else {
-        fgets(str, sizeof(str), file);
-        printf("%d", count_sentences(str));
-        fclose(file);
+  int flag = 0;
+  char name[1000];
+  fgets(name, sizeof(name), stdin);
+  name[strcspn(name, "\n")] = '\0';
+  char str[1000] = "";
+  char buffer[1000];
+  FILE *file;
+  file = fopen(name, "r");
+  if (file == NULL) {
+    flag = 1;
+  } else {
+    while (fgets(buffer, sizeof(buffer), file) != NULL) {
+      strcat(str, buffer);
     }
-    if (flag == 1) {
-        printf("n/a");
-    }
-    return flag;
+    printf("%d", count_sentences(str));
+    fclose(file);
+  }
+  if (flag == 1) {
+    printf("n/a");
+  }
+  return flag;
 }
